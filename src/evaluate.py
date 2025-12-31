3ccb9457a0869029620aba723ac7cf1763af4241
YQ <132048732+yq001218@users.noreply.github.com>
2025-12-31 09:32:11 +0800

    Update evaluate.py

--- src/evaluate.py
+++ src/evaluate.py
@@ -1,1 +1,10 @@
 
+from src.metrics import rmse, mae, r2, psi
+
+def evaluate(y_true, y_pred):
+    return {
+        "RMSE": rmse(y_true, y_pred),
+        "MAE": mae(y_true, y_pred),
+        "R2": r2(y_true, y_pred),
+        "PSI": psi(y_true, y_pred)
+    }
